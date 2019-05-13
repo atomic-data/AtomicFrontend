847a77d9e3096efa72951eab0aa0e6a8f53e8fff
Ptit-Biscuit <vincent.brebion@epsi.fr>
2019-05-13 10:24:57 +0200

    Fix bateau

--- src/app/services/plop.service.ts
+++ src/app/services/plop.service.ts
@@ -1,7 +1,9 @@
 import {SimpleData} from './simple-data.interface';
 import {TableData} from './table-data.interface';
 import {Label} from './label.interface';
+import {Injectable} from '@angular/core';
 
+@Injectable({providedIn: 'root'})
 export class PlopService {
   public userDescription: string;
   public overviewData: SimpleData[];
